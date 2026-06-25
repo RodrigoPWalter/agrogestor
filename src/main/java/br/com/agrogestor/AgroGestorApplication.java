45806649d2389531e40962e5e424e369a84cc76d
RodrigoPWalter <rodrigowalter1234@icloud.com>
2026-06-25 19:12:00 -0300

    chore: prepara ambiente local e estrutura do Spring

---
+++ src/main/java/br/com/agrogestor/AgroGestorApplication.java
@@ -0,0 +1,12 @@
+package br.com.agrogestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroGestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroGestorApplication.class, args);
+    }
+}
